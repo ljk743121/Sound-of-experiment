c712a279fdd33168dcda565cfdbe07f0f99b5df9
Li Peter <ljk743121@outlook.com>
2025-01-29 21:39:54 +0800

    Update AboutUsDialog.vue

--- components/AboutUsDialog.vue
+++ components/AboutUsDialog.vue
@@ -20,33 +20,11 @@
           </p>
           <p>
             这个项目的代码是开源的，你可以在
-            <NuxtLink to="https://github.com/SMS-COSMO/the1068fm" class="font-bold">
+            <NuxtLink to="https://github.com/ljk743121/the1068fm" class="font-bold">
               <span>这里</span>
             </NuxtLink>
             查看源代码。期待各位同学的反馈！
           </p>
-        </section>
-        <LogosCosmo class="mx-auto my-0 w-[80%]" />
-        <section class="prose">
-          <p>
-            <b>COSMO智慧校园平台运研中心</b>是深圳中学七大校级学生组织之一，也是最年轻的校级学生组织。欢迎大家通过下方链接关注我们！
-          </p>
-          <section class="flex gap-4">
-            <NuxtLink
-              to="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg2NzUzOTIyOA==&scene=124#wechat_redirect"
-              class="text-gray-600 flex justify-center items-center gap-1"
-            >
-              <WeChatLogo />
-              微信公众号
-            </NuxtLink>
-            <NuxtLink to="https://github.com/SMS-COSMO/" class="text-gray-600 flex justify-center items-center gap-1">
-              <span class="icon-[radix-icons--github-logo]" />
-              GitHub
-            </NuxtLink>
-            <NuxtLink to="https://the1068.pictures/" class="text-gray-600 flex justify-center items-center gap-1 font-serif">
-              the1068
-            </NuxtLink>
-          </section>
         </section>
       </div>
       <UiDialogFooter>
