a27c13f5ab47ebef8c8dfde1e1ea664f50fe0908
Li Peter <ljk743121@outlook.com>
2025-08-11 14:01:11 +0800

    chore(ui): update shadcn-vue components
    - Add Accordion
    - Update Tooltip
    - Modify tailwind config to use Reka UI

--- app/components/ui/tooltip/TooltipTrigger.vue
+++ app/components/ui/tooltip/TooltipTrigger.vue
@@ -1,11 +1,12 @@
+<script setup lang="ts">
+import type { TooltipTriggerProps } from "reka-ui"
+import { TooltipTrigger } from "reka-ui"
+
+const props = defineProps<TooltipTriggerProps>()
+</script>
+
 <template>
   <TooltipTrigger v-bind="props">
     <slot />
   </TooltipTrigger>
 </template>
-
-<script setup lang="ts">
-import { TooltipTrigger, type TooltipTriggerProps } from 'radix-vue';
-
-const props = defineProps<TooltipTriggerProps>();
-</script>
