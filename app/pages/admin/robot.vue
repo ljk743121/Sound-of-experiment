96774306f38c71fd6125b936e38da58ac061f5d4
Li Peter <ljk743121@outlook.com>
2025-09-14 16:01:38 +0800

    fix(robot): page and ui

--- app/pages/admin/robot.vue
+++ app/pages/admin/robot.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-[calc(100svh-4rem)] flex-col gap-4 p-4">
     <div class="flex w-full max-w-sm items-center gap-1.5">
-      <Button type="submit" :disabled="isPending" @click="refetch()">
-        <Icon v-if="isPending" name="lucide:loader-circle" class="mr-2 animate-spin" />
+      <Button type="submit" :disabled="isRefetching" @click="refetch()">
+        <Icon v-if="isRefetching" name="lucide:loader-circle" class="mr-2 animate-spin" />
         获取{{ userStore.id }}的机器人令牌
       </Button>
     </div>
@@ -18,10 +18,11 @@ definePageMeta({
 const { $trpc } = useNuxtApp();
 const userStore = useUserStore();
 
-const { data, isPending, refetch } = useQuery({
+const { data, isRefetching, refetch } = useQuery({
   queryFn: () => $trpc.user.getRobotToken.query(),
   queryKey: ["user.getRobotToken"],
   refetchOnWindowFocus: false,
+  refetchInterval: false,
   enabled: false,
 });
 </script>
